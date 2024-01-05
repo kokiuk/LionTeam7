11358af6e4475fa276dc7c4b20b848f8e459ca34
kokiuk <kgukid38@gmail.com>
2024-01-05 17:17:35 +0900

    netflix project

---
+++ Nexflix/src/main/java/com/example/nexflix/NexflixApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexflixApplication.class, args);
+    }
+
+}
---
+++ Nexflix/src/test/java/com/example/nexflix/NexflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
