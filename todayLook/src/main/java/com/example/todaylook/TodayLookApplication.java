6d75b1cce00c737eb5a159eccb3d126d7bd470ae
kokiuk <kgukid38@naver.com>
2024-01-25 17:17:07 +0900

    todayLook update

---
+++ todayLook/src/main/java/com/example/todaylook/TodayLookApplication.java
@@ -0,0 +1,13 @@
+package com.example.todaylook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayLookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayLookApplication.class, args);
+    }
+
+}
