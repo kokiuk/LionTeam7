6d187dacc28701ae992b9c0f6eedb00ac6d8f630
kokiuk <kgukid38@naver.com>
2024-01-26 17:19:56 +0900

    Article Entity 추가

--- todayLook/src/main/java/com/example/todaylook/entity/Article.java
+++ todayLook/src/main/java/com/example/todaylook/entity/Article.java
@@ -1,4 +1,13 @@
 package com.example.todaylook.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
