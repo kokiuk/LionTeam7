21b9bfc0aa1aa1e8ad1f97bd1e0c9490cb053d38
kokiuk <kgukid38@naver.com>
2024-02-14 16:14:18 +0900

    CRUD 준비

--- todayLook/src/main/java/com/example/todaylook/controller/ArticleController.java
+++ todayLook/src/main/java/com/example/todaylook/controller/ArticleController.java
@@ -1,20 +1,33 @@
 package com.example.todaylook.controller;
 
+import com.example.todaylook.dto.ArticleDto;
 import com.example.todaylook.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/article")
 public class ArticleController {
     private final ArticleService articleService;
 
-    @GetMapping("/article")
-    public String readOne(){
-        
+    //Read
+    @GetMapping("/{id}}")
+    public String readOne(
+            @PathVariable
+            Long id,
+            Module module
+    ){
+
         return "";
     }
+
+    //Create
+
+    //Update
+
+    //Delete
 }
